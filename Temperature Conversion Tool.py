cd48f23ad5a58a9a3db6760e619efbf754591f4c
Your Name <you@example.com>
2024-06-08 12:30:01 +0530

    New File

---
+++ Temperature Conversion Tool.py
@@ -0,0 +1,23 @@
+#Asmi Purarkar
+ 
+#Temperature Conversion Tool
+def cel(n):
+    temp=(n-32)*5/9
+    print('celcius temperature is:',temp)
+    return temp
+    
+def fah(n):
+    temp=(9/5)*n+32
+    print('fahrenheit tempeature is:',temp)
+    return temp
+
+while ( next != 'no' ):
+    ch=input("Enter conversion choice(cel or fah):")
+    n=int(input("Enter temperature to convert:"))
+    if (ch == 'cel'):
+        cel(n)
+    elif (ch == 'fah'):
+        fah(n)
+    else :
+        print("invalid choice")
+    next=input("If you want to continue then yes else no:")
