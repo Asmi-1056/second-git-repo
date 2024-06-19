d0624940e2181a98a6194da8fe7361daf85a6293
Asmi Purarkar <asmipurarkar@gmail.com>
2024-06-19 19:38:47 +0530

    Basic Calculator.py

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,33 +1,33 @@
 #Asmi Purarkar
 #Basic Calculator
 
-def add(a,b):
+def Add(a,b):
     return a+b
     
-def sub(a,b):
+def Sub(a,b):
     return a-b
 
-def mul(a,b):
+def Mul(a,b):
     return a*b
 
-def div(a,b):
+def Div(a,b):
     return a/b
 
-while ( next != 'no' ):
-    ch=input("Enter choice(add,sub,mul,div):")
+while ( new != 'no' ):
+    ch=input("Enter choice(Add,Sub,Mul,Div):")
     a=int(input("Enter number:"))
     b=int(input("Enter number:"))
-    if (ch == 'add'):
-        print("addition is:",add(a,b))
-    elif (ch == 'sub'):
-        print("subtraction is:",sub(a,b))
-    elif (ch == 'mul'):
-        print("multiplication is:",mul(a,b))
-    elif (ch == 'div'):
+    if (ch == 'Add'):
+        print("Addition is:",Add(a,b))
+    elif (ch == 'Sub'):
+        print("Subtraction is:",Sub(a,b))
+    elif (ch == 'Mul'):
+        print("Multiplication is:",Mul(a,b))
+    elif (ch == 'Div'):
         if (b==0):
-            print("can not divide by zero")
+            print("Cannot divide by zero")
         else:
-            print("division is:",div(a,b))
+            print("Division is:",Div(a,b))
     else :
-        print("invalid choice")
-    next=input("If you want to continue then yes else no:")
+        print("Invalid choice")
+    new=input("If you want to continue then yes else no:")
